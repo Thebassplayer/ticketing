8897421e8203b2778b5a1652d2bf3a99938fdc54
Roy Lopez <ryhsu2@gmail.com>
2025-01-10 16:14:29 +0100

    refactor User model to include UserDoc interface and improve type safety in Mongoose schema

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,16 +1,23 @@
 import mongoose, { Schema } from "mongoose";
 
+// Interface that describes the properties that are required to create a new User
 interface UserAttrs {
   email: string;
   password: string;
 }
 
 // Interface that describes the properties that a User model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
 }
 
-const userSchema = new Schema<UserAttrs>({
+// Interface that describes the properties that a User instance has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema({
   email: {
     type: String,
     required: true,
@@ -25,6 +32,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 export { User };
