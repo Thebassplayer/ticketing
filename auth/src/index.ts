fe7cde18be217a0f92cdd602e15090ef7cc83b06
Roy Lopez <ryhsu2@gmail.com>
2025-01-10 10:52:34 +0100

    add error handling middleware and validate signup input

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,16 +1,19 @@
 import express from "express";
-import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
+import { currentUserRouter } from "./routes/currentUser";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 app.use(express.json());
 
-app.use(currentUserRouter);
 app.use(signinRouter);
+app.use(currentUserRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
