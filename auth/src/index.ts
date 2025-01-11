b97c3c988103eb068df7b94f4c728b7c91474f70
Roy Lopez <ryhsu2@gmail.com>
2025-01-11 19:21:12 +0100

    add cookie-session middleware for enhanced session management and trust proxy configuration

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express, { ErrorRequestHandler } from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { signinRouter } from "./routes/signin";
 import { currentUserRouter } from "./routes/currentUser";
@@ -10,7 +11,14 @@ import { errorHandler } from "./middlewares/errorHandler";
 import { NotFoundError } from "./errors/notFoundError";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(signinRouter);
 app.use(currentUserRouter);
