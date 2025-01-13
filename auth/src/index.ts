6f3edecf1b53830ed015a8a135dfec88b4b560e3
Roy Lopez <ryhsu2@gmail.com>
2025-01-13 11:09:22 +0100

    refactor server setup by moving app configuration to a separate file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,5 @@
-import express, { ErrorRequestHandler } from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { signinRouter } from "./routes/signin";
-import { currentUserRouter } from "./routes/currentUser";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/errorHandler";
-import { NotFoundError } from "./errors/notFoundError";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(signinRouter);
-app.use(currentUserRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler as ErrorRequestHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
