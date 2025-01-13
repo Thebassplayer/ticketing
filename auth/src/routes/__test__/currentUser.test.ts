a79d65fa9e52806efa2baec4b77bf3e3496b51b2
Roy Lopez <ryhsu2@gmail.com>
2025-01-13 14:43:35 +0100

    add test for unauthenticated user response in current user endpoint; remove requireAuth middleware

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -17,4 +17,12 @@ describe("GET /api/users/currentuser", () => {
       expect(response.body.currentUser.email).toEqual("test@email.com");
     }
   });
+  it("responds with null if not authenticated", async () => {
+    const response = await request(app)
+      .get("/api/users/currentuser")
+      .send()
+      .expect(200);
+
+    expect(response.body.currentUser).toBeNull();
+  });
 });
