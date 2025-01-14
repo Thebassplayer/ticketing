c488c539bdce78b2a23443c044785c766d21570a
Roy Lopez <ryhsu2@gmail.com>
2025-01-14 13:15:42 +0100

    add MongoDB connection handling and create ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import cookieSession from "cookie-session";
 
 import { errorHandler, NotFoundError } from "@rldtickets/common";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -13,6 +14,8 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(createTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
