01887693ef91689d7778ee0b81e2a0dbf2b8a30d
Roy Lopez <ryhsu2@gmail.com>
2025-01-25 18:33:42 +0100

    feat: implement TicketUpdatedPublisher and integrate with ticket update route

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -18,7 +18,7 @@ const start = async () => {
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-    await mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(process.env.MONGO_URI!, {});
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error(err);
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
 } from "@rldtickets/common";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
+import { TicketUpdatedPublisher } from "../events/publishers/ticketUpdatedPublisher";
+import { natsWrapper } from "../natsWrapper";
 
 const router = express.Router();
 
@@ -36,6 +38,13 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
