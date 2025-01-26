39b1dd9b30498b8716a146c28fa25cfe636f70fc
Roy Lopez <ryhsu2@gmail.com>
2025-01-26 13:41:58 +0100

    feat: implement CRUD routes for orders service

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,10 +3,10 @@ import "express-async-errors";
 import cookieSession from "cookie-session";
 
 import { errorHandler, NotFoundError, currentUser } from "@rldtickets/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { createOrdersRouter } from "./routes/new";
+import { showOrdersRouter } from "./routes/show";
+import { indexOrdersRouter } from "./routes";
+import { deleteOrdersRouter } from "./routes/delete";
 
 const app = express();
 app.set("trust proxy", true);
@@ -20,10 +20,10 @@ app.use(
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(createOrdersRouter);
+app.use(showOrdersRouter);
+app.use(indexOrdersRouter);
+app.use(deleteOrdersRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
