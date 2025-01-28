704b8c85097d436404c25481673413bce1d87841
Roy Lopez <ryhsu2@gmail.com>
2025-01-28 15:28:07 +0100

    test: add version increment test for Ticket model on multiple saves

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -26,4 +26,21 @@ describe("Ticket Model", () => {
     // Save the second fetched ticket and expect an error
     await expect(secondInstance!.save()).rejects.toThrow();
   });
+
+  it("increments the version number on multiple saves", async () => {
+    const ticket = Ticket.build({
+      title: "concert",
+      price: 20,
+      userId: "123",
+    });
+
+    await ticket.save();
+    expect(ticket.version).toEqual(0);
+
+    await ticket.save();
+    expect(ticket.version).toEqual(1);
+
+    await ticket.save();
+    expect(ticket.version).toEqual(2);
+  });
 });
