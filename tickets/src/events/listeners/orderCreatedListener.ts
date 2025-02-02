a4ec0ce72c472a9832651f6ea5e29946117d2578
Roy Lopez <ryhsu2@gmail.com>
2025-02-02 18:52:21 +0100

    feat: implement OrderCreatedListener to handle order creation events and update ticket reservation

---
+++ tickets/src/events/listeners/orderCreatedListener.ts
@@ -0,0 +1,24 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@rldtickets/common";
+import { queueGroupName } from "./queueGroupName";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+    // If no ticket, throw an error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+    // Save the ticket
+    await ticket.save();
+    // Ack the message
+    msg.ack();
+  }
+}
