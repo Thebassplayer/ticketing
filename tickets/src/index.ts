98b41354fb2d9744493c23a2b37d81248916dabd
Roy Lopez <ryhsu2@gmail.com>
2025-02-03 13:11:57 +0100

    feat: add OrderCreatedListener and OrderCancelledListener to handle order events

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./natsWrapper";
+import { OrderCreatedListener } from "./events/listeners/orderCreatedListener";
+import { OrderCancelledListener } from "./events/listeners/orderCancelledListener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -31,6 +33,10 @@ const start = async () => {
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI!, {});
     console.log("Connected to MongoDB");
   } catch (err) {
