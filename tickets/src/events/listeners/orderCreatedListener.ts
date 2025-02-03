ae5a0e5b6fa40e54d9fd406f64bd98c4a2620abf
Roy Lopez <ryhsu2@gmail.com>
2025-02-03 12:42:37 +0100

    feat: publish ticket:updated event in OrderCreatedListener after ticket update

--- tickets/src/events/listeners/orderCreatedListener.ts
+++ tickets/src/events/listeners/orderCreatedListener.ts
@@ -2,6 +2,7 @@ import { Listener, OrderCreatedEvent, Subjects } from "@rldtickets/common";
 import { queueGroupName } from "./queueGroupName";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticketUpdatedPublisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -18,6 +19,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     ticket.set({ orderId: data.id });
     // Save the ticket
     await ticket.save();
+    // Publish the ticket:updated event
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
+
     // Ack the message
     msg.ack();
   }
