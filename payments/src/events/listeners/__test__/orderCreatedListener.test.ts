4dd7042149e66cbf0f0ae80c3007ca798f6ea176
Roy Lopez <ryhsu2@gmail.com>
2025-02-20 12:54:09 +0100

    test: add unit tests for OrderCreatedListener

---
+++ payments/src/events/listeners/__test__/orderCreatedListener.test.ts
@@ -0,0 +1,47 @@
+import { natsWrapper } from "../../../natsWrapper";
+import { OrderCreatedListener } from "../orderCreatedListener";
+import { OrderCreatedEvent, OrderStatus } from "@rldtickets/common";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../../models/order";
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const data: OrderCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    userId: "123",
+    status: OrderStatus.Created,
+    expiresAt: "123",
+    ticket: {
+      id: "123",
+      price: 10,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it("replicates the order info", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order).toBeDefined();
+  expect(order!.price).toEqual(data.ticket.price);
+});
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
